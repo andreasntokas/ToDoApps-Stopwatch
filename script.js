ccffe17e371bf7a7b95556748ef6fa9acacbf7ae
Andreas Ntokas <ntokas.an@gmail.com>
2022-02-19 22:39:44 +0200

    Final

---
+++ script.js
@@ -0,0 +1,67 @@
+//Global Variables
+const time_el = document.querySelector(".watch .time");
+const placeholder = document.querySelector(".watch");
+
+const start_btn = document.getElementById("start");
+const stop_btn = document.getElementById("stop");
+const reset_btn = document.getElementById("reset");
+let seconds = 0;
+let interval = null;
+
+//Event Listeners
+
+start_btn.addEventListener("click", start);
+stop_btn.addEventListener("click", stop);
+reset_btn.addEventListener("click", reset);
+
+//Update the timer
+function timer () {
+    seconds++;
+
+    //Format our time
+    let hrs = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds - (hrs *3600))/ 60);
+    let secs = seconds % 60;
+
+    if (secs<10) {
+        secs = "0" + secs;
+    }
+    if (mins<10) {
+        mins = "0" + mins;
+    }
+    if (hrs<10) {
+        hrs = "0" + hrs;
+    }
+
+    time_el.innerText = `${hrs}:${mins}:${secs}`;
+
+    //Change width when hours reach 100
+    if (seconds>=3600*100) {
+        document.querySelector(".watch").style.maxWidth = "420px";
+    }
+    //Stop timer right before 1000 hours
+    if (seconds>3599.998*1000)
+     {  
+        clearInterval(interval);
+    }
+}
+//Buttons
+function start () {
+    if (interval) {
+        return
+    }
+
+    interval = setInterval(timer, 1000);
+}
+
+function stop () {
+    clearInterval(interval);
+    interval = null;
+}
+
+function reset () {
+    stop();
+    seconds = 0;
+    time_el.innerText = "00:00:00";
+    document.querySelector(".watch").style.maxWidth = "360px";
+}
